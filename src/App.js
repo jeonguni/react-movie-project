b8de539b87e7e4b86df22685193c8507ecb9e580
ejy55n <ejy55n2@gmail.com>
2023-10-18 17:19:36 +0900

    feat: react router install

--- src/App.js
+++ src/App.js
@@ -1,46 +1,5 @@
-import { useEffect, useState } from 'react';
-
 function App() {
-  const [loading, setLoading] = useState(true);
-  const [movies, setMovies] = useState([]);
-  const getMovies = async () => {
-    const json = await (await fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)).json();
-    setMovies(json.data.movies);
-    setLoading(false);
-  };
-  useEffect(() => {
-    getMovies();
-    // fetch(`https://yts.mx/api/v2/list_movies.json?minimum_rating=8.8&sort_by=year`)
-    //   .then(response => response.json())
-    //   .then(json => {
-    //     setMovies(json.data.movies);
-    //     setLoading(false);
-    //   });
-  }, []);
-  console.log(movies);
-  return (
-    <div>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : (
-        <div>
-          {movies.map(movie => (
-            <div key={movies.id}>
-              <img src={movie.medium_cover_image} />
-              <h2>{movie.title}</h2>
-              <p>{movie.summary}</p>
-              <ul>
-                {movie.genres.map(genre => (
-                  <li key={genre}>{genre}</li>
-                ))}
-              </ul>
-            </div>
-          ))}
-        </div>
-      )}
-      <h1></h1>
-    </div>
-  );
+  return null;
 }
 
 export default App;
